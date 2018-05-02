a7a8fef8665c6ebe770a5a75a87f5a8bb8c9f2b9
juliet26 <36687161+juliet26@users.noreply.github.com>
2018-05-02 13:50:40 -0500

    nuevo repo

---
+++ NavigationPage/NavigationPage/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace NavigationPage
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+
+			InitializeComponent();
+            MainPage = new NavigationPage(new MainPage());
+
+            var navigationPage = new NavigationPage(new MainPage());
+            navigationPage.BarBackGroundColor = Color.Blue;
+            navigationPage.BarTextColor = Color.White;
+            MainPage = navigationPage;
+
+            
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
