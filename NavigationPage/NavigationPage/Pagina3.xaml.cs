3d051c608e93af4e3f97c8045c87d20e74a52353
juliet26 <36687161+juliet26@users.noreply.github.com>
2018-05-03 00:37:38 -0500

    terminado
    añadiendo últimas páginas

--- NavigationPage/NavigationPage/Pagina3.xaml.cs
+++ NavigationPage/NavigationPage/Pagina3.xaml.cs
@@ -15,6 +15,21 @@ namespace NavigationPage
 		public Pagina3 ()
 		{
 			InitializeComponent ();
-		}
-	}
+            this.btnInicio.Clicked += BtnInicio_Clicked;
+            this.btnSiguiente.Clicked += BtnSiguiente_Clicked;
+            this.btnAnterior.Clicked += BtnAnterior_Clicked;
+        }
+        private async void BtnInicio_Clicked(object sender, EventArgs e)
+        {
+            await NavigationPage.PopToRootAsync();
+        }
+        private async void BtnSiguiente_Clicked(object sender, EventArgs e)
+        {
+            await NavigationPage.PushModalAsync(new Pagina4());
+        }
+        private async void BtnAnterior_Clicked(object sender, EventArgs e)
+        {
+            await NavigationPage.PopAsync();
+        }
+    }
 }
